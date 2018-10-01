4faa8cbe154654e7f9a047c7d8e436c17bcadb71
Allister Rooke <allisterg@gmail.com>
2018-10-01 12:01:35 -0400

    did step 1 of namespacing

--- scripts/Item.js
+++ scripts/Item.js
@@ -3,8 +3,25 @@
 
 const Item = (function() {
   
+  const validateName = function(name) {
+    if (!name) {
+      throw 'Name does not exist';
+    }
+  };
+
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name,
+      checked: false
+
+    };
+  };
+
+
   return {
-    // empty object
+    validateName,
+    create
   };
 
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
 /* global shoppingList, cuid */
+'use strict';
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  
+
 });
 
 
