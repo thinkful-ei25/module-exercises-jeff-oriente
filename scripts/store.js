5fc6e7908fb6653161fb324560f8ea9d5a12f1b9
jeff oriente <jdanieloriente@gmail.com>
2018-10-01 11:24:35 -0400

    Did part 1 of exercise

---
+++ scripts/store.js
@@ -0,0 +1,18 @@
+const store = (function () {
+    const foo = 'bar';
+        const items = [
+          { id: cuid(), name: 'apples', checked: false },
+          { id: cuid(), name: 'oranges', checked: false },
+          { id: cuid(), name: 'milk', checked: true },
+          { id: cuid(), name: 'bread', checked: false }
+        ];
+        let hideCheckedItems = false;
+        let searchTerm = '';
+
+        return {
+            items,
+            hideCheckedItems,
+            searchTerm
+        }
+      
+}() );
